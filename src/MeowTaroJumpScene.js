171031330aeda2ff3fc731474dff28dcb9bba0a6
yosema44 <201330158+yosema44@users.noreply.github.com>
2025-07-12 16:28:53 +0700

    2

--- src/MeowTaroJumpScene.js
+++ src/MeowTaroJumpScene.js
@@ -10,9 +10,18 @@ export default class MeowTaroJumpScene extends Phaser.Scene {
 
 	preload() {
 		this.load.image("background", "images/backgroundColorFall.png")
+		this.load.spritesheet("idle", "Idle.png", {
+			frameWidth: 48,
+			frameHeight: 48,
+		})
+		this.load.spritesheet("Walk", "Walk.png", {
+			frameWidth: 48,
+			frameHeight: 48,
+		})
 	}
 	create() {
 		this.add.image(240, 320, "background")
+		this.physics.add.sprite(240, 320, "idle")
 	}
 	update() {
 
