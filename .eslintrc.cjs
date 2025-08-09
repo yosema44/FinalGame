c216151d5617684a7c6edc10ae27b728c22b31d1
yosema44 <201330158+yosema44@users.noreply.github.com>
2025-08-09 15:09:48 +0700

    3

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,11 @@ module.exports = {
 		browser: true,
 		es2021: true,
 	},
-	extends: 'eslint:recommended',
+	extends: "eslint:recommended",
 	overrides: [],
 	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
+		ecmaVersion: "latest",
+		sourceType: "module",
 	},
 	rules: {},
 }
--- src/MeowTaroJumpScene.js
+++ src/MeowTaroJumpScene.js
@@ -7,7 +7,11 @@ export default class MeowTaroJumpScene extends Phaser.Scene {
 	init() {
 		this.player = null
 		this.cursors = null
-		this.isJUmping = false
+		this.isJumping = false
+		this.clownfish = undefined
+		this.salmon = undefined
+		this.solarfish = undefined
+		this.mudfish = undefined
 	}
 
 	preload() {
@@ -16,27 +20,102 @@ export default class MeowTaroJumpScene extends Phaser.Scene {
 		this.load.image("salmon", "images/Salmon.png")
 		this.load.image("solarfish", "images/Solarfish.png")
 		this.load.image("mudfish", "images/mudfish.png")
-		this.load.image("tile", "images/block_yellow.png")
-		this.load.spritesheet("idle", "images/Idle.png", {
-			frameWidth: 48,
-			frameHeight: 48,
-		})
-		this.load.spritesheet("Walk", "images/Walk.png", {
-			frameWidth: 48,
-			frameHeight: 48,
-		})
+		this.load.image("platform", "images/block_yellow.png") 
+		this.load.spritesheet("idle", "images/Idle.png", { frameWidth: 48, frameHeight: 48, })
+		this.load.spritesheet("walk", "images/Walk.png", { frameWidth: 48, frameHeight: 48, })
 	}
 	create() {
 		this.add.image(240, 320, "background")
-
+		this.add.image(240, 320, "platform").setScale(0.5)
 		this.player = this.physics.add.sprite(240, 320, "idle")
 		this.player.setCollideWorldBounds(true)
 		this.player.setBounce(0.2)
-
+		this.cursors = this.input.keyboard.createCursorKeys()
 		this.createAnimations()
+		this.player.anims.play("idle", true)
+		this.createPlatform()
+			
 	}
+	createPlatform() {
+		this.platform = this.physics.add.staticGroup()
+		
+		let YLevel = 100
+		let lastX = 240;
+
+		for (let i = 0; i < 7; i++) {
+			let randomX;
+			let attempts = 0;
+
+		do {
+			randomX = Math.floor(Math.random() * 420) + 30;
+			attempts++;
+		} while (attempts < 10 && Math.abs(randomX - lastX) < 80);
+
+		if (Math.abs(randomX - lastX) < 80) {
+			if (lastX < 240) {
+				randomX = Math.min(450, lastX + 100 + Math.random() * 50);
+			} else {
+				randomX = Math.max(30, lastX - 100 - Math.random() * 50);
+			}
+		}
+
+		this.platform.create(randomX, YLevel, "platform")
+		lastX = randomX;
+		const nextYLevel = YLevel + Math.floor(Math.random() * 20) + 60;
+		YLevel = Math.min(nextYLevel, 480);
+		}
+	}
+
 	update() {
 		const onGround = this.player.body.blocked.down || this.player.body.touching.down
+
+		if(onGround && this.isJumping) {
+			this.isJumping = false
+			this.player.setScale(1)
+			this.player.setRotation(0)
+		}
+
+		if (this.cursors.left.isDown) {
+  		this.player.setVelocity(-70, 0);
+		}
+
+		if (this.cursors.right.isDown) {
+ 		 this.player.setVelocityX(70);
+		}
+
+		if(this.cursors.left.isDown) {
+			this.player.setVelocityX(-160)
+			this.player.setFlipX(true)
+			if (this.isJumping) {
+				this.player.anims.play("jump", true)
+			} else {
+				this.player.anims.play("walk", true)
+			}
+		} 
+		else if (this.cursors.right.isDown){
+			this.player.setVelocityX(160)
+			this.player.setFlipX(false)
+			if (this.isJumping) {
+				this.player.anims.play("jump", true)
+			} else {
+				this.player.anims.play("walk", true)
+			}
+		}
+		else {
+			this.player.setVelocityX(0)
+			if (this.isJumping) {
+				this.player.anims.play("jump", true)
+			} else {
+				this.player.anims.play("walk", true)
+			}
+		}
+
+		if (this.cursors.space.isDown && onGround && !this.isJumping) {
+			this.player.setVelocityY(-225)
+			this.isJumping = true
+			this.player.setScale(1.1)
+			this.player.anims.play("jump", true)
+		}
 	}
 	createAnimations() {
 		this.anims.create({
